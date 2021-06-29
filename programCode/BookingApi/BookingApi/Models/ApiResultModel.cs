61c4908a4da2b1387b7783dc148bc6adc2b8078e
JasonSGuan <guanshijie14@gmail.com>
2021-06-29 18:48:46 +0800

    添加登陆模块

---
+++ programCode/BookingApi/BookingApi/Models/ApiResultModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookingApi.Models
+{
+    public class ApiResultModel
+    {
+        public string Status;
+        public object Data;
+        public string Message;
+    }
+}
